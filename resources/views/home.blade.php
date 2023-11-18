8c1139df654fe71a6c2fb3819951d23149e2f6d6
estherpinos <estherpinos1@gmail.com>
2023-11-18 12:41:22 +0100

    Add [cards]

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,28 @@
 
 @section('content')
 
+<div class="container" id="container-main">
+    <div v-for="(item, index) in menuMain" :key="index" class="product">
+      <img :src="getImagePath(item.primaryImage)">
+
+      <span class="saldi" v-if="item.discount">{{ item.discount }}</span>
+      <span class="sostenibilità" v-if="item.sostenibilita">{{item.sostenibilita}}</span>
+      <div class="product-2">
+        <img :src="getImagePath(item.secondaryImage)">
+      </div>
+      <div class="heart"><i class="fa-solid fa-heart"></i></div>
+
+
+      <div class="text-foto">
+        <p class="marca">{{ item.marca }}</p>
+        <h4>{{ item.modello }}</h4>
+        <span class="prezzo-saldi">{{ item.lastPrice }} &euro;</span>
+        <span class="prezzo" v-if="item.fullPrice"> {{ item.fullPrice }} &euro;</span>
+
+      </div>
+    </div>
+
+</div>
+
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $products = config('prodotti');
+    return view('home', compact('products'));
 
 })->name('home');
+
