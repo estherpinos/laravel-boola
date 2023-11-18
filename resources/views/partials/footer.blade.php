f73eea2796d6ef9173e1b4d5fab2b534c40699e9
estherpinos <estherpinos1@gmail.com>
2023-11-18 11:47:51 +0100

    Add [footer e images]

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Footer</title>
+</head>
+<body>
+
+    <footer>
+        <section class="section-footer">
+          <div class="container">
+            <div class="menu-footer">
+              <p class="text-footer">Boolando S.r.l</p>
+             <nav>
+              <ul>
+                <li>
+                  <a>text</a>
+                </li>
+
+              </ul>
+            </nav>
+            </div>
+            <div class="social-footer">
+              <p class="text-footer">Trovaci anche su</p>
+              <nav>
+                <ul>
+                  <li><a>icon</a></li>
+
+
+                </ul>
+              </nav>
+            </div>
+
+          </div>
+
+        </section>
+      </footer>
+
+</body>
+</html>
